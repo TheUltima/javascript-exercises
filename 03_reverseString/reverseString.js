606af3900185d783f19e7ff1b557ec7624bb1c77
TheUltima <xxdeatjerzxx@gmail.com>
2024-03-02 23:38:32 -0500

    Finish 03 with two different methods

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,18 @@
-const reverseString = function() {
+// const reverseString = function (string) {
+//   let stringInReverse = "";
+//   for (let i = string.length - 1; i >= 0; i--) {
+//     stringInReverse += string[i];
+//   }
 
+//   return stringInReverse;
+// };
+
+const reverseString = function (string) {
+  let stringInReverse = "";
+  for (let i = 1; i <= string.length; i++) {
+    stringInReverse += string.at(-i);
+  }
+  return stringInReverse;
 };
 
 // Do not edit below this line
