53b1c0aa11490887d75ad5bc8738886bd1478132
TheUltima <xxdeatjerzxx@gmail.com>
2024-03-03 03:36:27 -0500

    Finish 07, extra comment on decimal place accuracy
    toFixed method returns a string, so it has to be converted to a number
    again. There are multiple ways to do so, and the way I used it is
    through the Unary Operator. After looking online however, toFixed is not
    recommended by some since it can be inaccurate at times, so multiply
    the number by a power of ten (The power determines how many decimal
    places you want to go), then Math.round the result, then divide by the
    same power of ten (The scientific notation can be used, and makes
    visualizing it simple).

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,17 @@
-const convertToCelsius = function() {
+//toFixed method returns a string, so it has to be converted to a number again. There are multiple ways to do so, and the way I used it is through the Unary Operator. After looking online however, toFixed is not recommended by some since it can be inaccurate at times, so multiply the number by a power of ten (The power determines how many decimal places you want to go), then Math.round the result, then divide by the same power of ten (The scientific notation can be used, and makes visualizing it simple).
+
+const convertToCelsius = function (degreesInFahrenheit) {
+  result = +((degreesInFahrenheit - 32) * (5 / 9)).toFixed(1);
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degreesInCelcius) {
+  result = Math.round(((9 / 5) * degreesInCelcius + 32) * 1e1) / 1e1;
+  return result;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
