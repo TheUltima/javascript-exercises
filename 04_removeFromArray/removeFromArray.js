6d03b537dfc6a09524f9d6808490ec65d9a8c126
TheUltima <xxdeatjerzxx@gmail.com>
2024-03-03 02:44:55 -0500

    Finish 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -21,13 +21,16 @@
 //   return result;
 // };
 
+/* The below function has a Rest Parameter (indicated with '...'), which is
+a way for the function to accept infinitely many, but optional arguments. This is done by populating an array of the same name as the Rest Parameter, from which we can then perform any array methods. This is a syntax created to remove the need of a previous way of turning a function's parameters into an array (look up MDN on the 'arguments object' and 'rest parameters'). Rest Parameters must always be the last parameter of a function.
+*/
+
 const removeFromArray = function (array, ...elementsToRemove) {
   let result = array.filter((currentElement) => {
     for (const elementToRemove of elementsToRemove) {
-      if (currentElement === elementToRemove) {
-        return;
-      }
+      if (currentElement === elementToRemove) return;
     }
+
     return currentElement;
   });
 
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function (int1, int2) {
+  let min = int1;
+  let max = int2;
 
+  if (int1 < 0 || int2 < 0) return "ERROR";
+  if (typeof int1 !== "number" || typeof int2 !== "number") return "ERROR";
+
+  if (min > max) {
+    min = int2;
+    max = int1;
+  }
+
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
